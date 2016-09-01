d27f0205e133cf4bae8955124591d052048802d1
Alex Kuang <eexteam@AK-Mac.local>
2016-08-31 17:55:29 -0700

    Hello, Android Multiscreen
    https://developer.xamarin.com/guides/android/getting_started/hello,andro
    id_multiscreen/hello,android_multiscreen_quickstart/

---
+++ Experiments/Experiments.Android/CallHistoryActivity.cs
@@ -0,0 +1,25 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Widget;
+using AndroidResource = Android.Resource;
+
+namespace Experiments.Android
+{
+	/// <summary>
+	/// Hello, Android Multiscreen
+	/// https://developer.xamarin.com/guides/android/getting_started/hello,android_multiscreen/hello,android_multiscreen_quickstart/
+	/// </summary>
+	[Activity(Label = "@string/callHistory")]
+	public class CallHistoryActivity : ListActivity
+	{
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			// Create your application here
+
+			var phoneNumbers = Intent.Extras.GetStringArrayList("phone_numbers") ?? new string[0];
+			this.ListAdapter = new ArrayAdapter<string>(this, AndroidResource.Layout.SimpleListItem1, phoneNumbers);
+		}
+	}
+}
--- Experiments/Experiments.Android/MainActivity.cs
+++ Experiments/Experiments.Android/MainActivity.cs
@@ -1,20 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
 using Android.App;
 using Android.Content;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
+using Android.Widget;
 using AndroidNet = Android.Net;
 
 namespace Experiments.Android
 {
-    [Activity(Label = "Phone Word", MainLauncher = true)]
+	[Activity(Label = "Phone Word", MainLauncher = true)]
     public class MainActivity : Activity
     {
-        protected override void OnCreate(Bundle bundle)
+		static readonly List<string> phoneNumbers = new List<string>();
+
+        protected override void OnCreate(Bundle savedInstanceState)
         {
-            base.OnCreate(bundle);
+            base.OnCreate(savedInstanceState);
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
@@ -23,6 +24,7 @@ namespace Experiments.Android
             EditText phoneNumberText = FindViewById<EditText>(Resource.Id.PhoneNumberText);
             Button translateButton = FindViewById<Button>(Resource.Id.TranslateButton);
             Button callButton = FindViewById<Button>(Resource.Id.CallButton);
+			Button callHistoryButton = FindViewById<Button>(Resource.Id.CallHistoryButton);
 
             // Disable the "Call" button
             callButton.Enabled = false;
@@ -51,7 +53,13 @@ namespace Experiments.Android
                 // On "Call" button click, try to dial phone number.
                 var callDialog = new AlertDialog.Builder(this);
                 callDialog.SetMessage("Call " + translatedNumber + "?");
+
                 callDialog.SetNeutralButton("Call", delegate {
+					// add dialed number to list of called numbers.
+					phoneNumbers.Add(translatedNumber);
+					// enable the Call History button
+					callHistoryButton.Enabled = true;
+
                     // Create intent to dial phone
                     var callIntent = new Intent(Intent.ActionCall);
                     callIntent.SetData(AndroidNet.Uri.Parse("tel:" + translatedNumber));
@@ -62,6 +70,13 @@ namespace Experiments.Android
                 // Show the alert dialog to the user and wait for response.
                 callDialog.Show();
             };
+
+			callHistoryButton.Click += (sender, e) =>
+			{
+				var intent = new Intent(this, typeof(CallHistoryActivity));
+				intent.PutStringArrayListExtra("phone_numbers", phoneNumbers);
+				StartActivity(intent);
+			};
         }
     }
 }
